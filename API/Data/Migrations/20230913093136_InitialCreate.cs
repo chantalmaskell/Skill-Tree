a81303bc268e24e8f51bc88b7e5a0d74e1482b33
scrambledegg33 <henry.grainger@gmail.com>
2023-09-13 11:07:59 +0100

    setting up c# .net backend

---
+++ API/Data/Migrations/20230913093136_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LearningInformationUnits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Url = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LearningInformationUnits", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LearningInformationUnits");
+        }
+    }
+}
---
+++ API/Entities/LearningInformationUnit.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class LearningInformationUnit
+    {
+       public int Id { get; set; }
+       public string Title { get; set; }
+       public string Description { get; set; }
+       public string Url { get; set; }
+    }
+}
